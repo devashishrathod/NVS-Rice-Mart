d3f1c6ec0cf6c80630d4a2407854b1df0587f02c
Devashish Rathod <rathod.devashish09@gmail.com>
2025-11-21 23:54:26 +0530

    images and create banner done

--- server/models/Banner.js
+++ server/models/Banner.js
@@ -6,7 +6,7 @@ const bannerSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     description: { type: String, trim: true },
     image: { type: String, default: DEFAULT_IMAGES.BANNER },
-    video: { type: String, required: true },
+    video: { type: String },
     isActive: { type: Boolean, default: true },
     isDeleted: { type: Boolean, default: false },
   },
--- server/services/products/deleteProduct.js
+++ server/services/products/deleteProduct.js
@@ -1,10 +1,13 @@
 const Product = require("../../models/Product");
 const { throwError, validateObjectId } = require("../../utils");
+const { deleteImage } = require("../uploads");
 
 exports.deleteProduct = async (id) => {
   validateObjectId(id, "Product Id");
   const result = await Product.findById(id);
   if (!result || result.isDeleted) throwError(404, "Product not found");
+  await deleteImage(result?.image);
+  result.image = null;
   result.isDeleted = true;
   result.isActive = false;
   result.updatedAt = new Date();
