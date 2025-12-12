5ac6adf3e855e745cbc5a0fa54e377c078393c29
Devashish Rathod <rathod.devashish09@gmail.com>
2025-12-13 01:50:39 +0530

    cart with stock map resolved

--- server/models/Product.js
+++ server/models/Product.js
@@ -10,8 +10,9 @@ const productSchema = new mongoose.Schema(
     brand: { type: String, required: true, trim: true },
     generalPrice: { type: Number, required: true },
     stockQuantity: { type: Number, required: true },
+    //  cartStock: { type: Number, default: 1 },
     SKU: { type: String, required: true, trim: true },
-    weightInKg: { type: Number, required: true },
+    weightInKg: { type: Number, required: true }, //Enum 10 , 25 kg
     description: { type: String, trim: true },
     image: { type: String, default: DEFAULT_IMAGES.PRODUCT },
     type: {
@@ -19,6 +20,7 @@ const productSchema = new mongoose.Schema(
       enum: [...Object.values(PRODUCT_TYPES)],
       default: PRODUCT_TYPES.GROCERY,
     },
+    isOutOfStock: { type: Boolean, default: false },
     isActive: { type: Boolean, default: true },
     isDeleted: { type: Boolean, default: false },
   },
--- server/services/carts/addOrUpdateItems.js
+++ server/services/carts/addOrUpdateItems.js
@@ -1,15 +1,26 @@
 const Cart = require("../../models/Cart");
 const Product = require("../../models/Product");
 const { calculateCartTotal } = require("../../helpers/carts");
-const { validateObjectId } = require("../../utils");
+const { validateObjectId, throwError } = require("../../utils");
 
 exports.addOrUpdateItem = async (userId, payload) => {
   let { productId, quantity } = payload;
   validateObjectId(productId, "Product Id");
   quantity = Number(quantity) || 1;
+  const product = await Product.findById(productId);
+  if (!product || product?.isDeleted) throwError(404, "Product not found");
+  if (product?.stockQuantity === 0 || product?.isOutOfStock) {
+    throwError(400, "Sorry, out of stock");
+  }
+  if (quantity > product?.stockQuantity) {
+    throwError(400, `Only ${product.stockQuantity} stock is remaining`);
+  }
+  let updatedProduct;
   let cart = await Cart.findOne({ userId, isPurchased: false });
   if (!cart) {
     cart = await Cart.create({ userId, items: [{ productId, quantity }] });
+    product.stockQuantity -= quantity;
+    updatedProduct = await product.save();
   } else {
     if (cart.isDeleted) cart.isDeleted = false;
     const itemIndex = cart.items.findIndex(
@@ -17,12 +28,30 @@ exports.addOrUpdateItem = async (userId, payload) => {
     );
     if (itemIndex > -1) {
       cart.items[itemIndex].quantity += quantity;
+      product.stockQuantity -= quantity;
+      updatedProduct = await product.save();
       if (cart.items[itemIndex].quantity <= 0) {
         cart.items.splice(itemIndex, 1);
+        product.stockQuantity += quantity;
+        updatedProduct = await product.save();
+        if (
+          updatedProduct &&
+          updatedProduct?.stockQuantity > 0 &&
+          updatedProduct?.isOutOfStock
+        ) {
+          product.isOutOfStock = false;
+          updatedProduct = await product.save();
+        }
       }
     } else {
       cart.items.push({ productId, quantity });
+      product.stockQuantity -= quantity;
+      updatedProduct = await product.save();
     }
+  }
+  if (updatedProduct && updatedProduct?.stockQuantity <= 0) {
+    product.isOutOfStock = true;
+    await product.save();
   }
   const productIds = cart.items.map((i) => i.productId);
   const products = await Product.find({ _id: { $in: productIds } });
