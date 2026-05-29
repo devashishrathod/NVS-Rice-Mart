55c233ff055471e4e866b774e62aee67842a15e9
Devashish Rathod <rathod.devashish09@gmail.com>
2026-05-29 23:18:14 +0530

    ne

--- server/services/locations/deleteLocation.js
+++ server/services/locations/deleteLocation.js
@@ -1,10 +1,13 @@
 const Location = require("../../models/Location");
 const { throwError, validateObjectId } = require("../../utils");
 
-exports.deleteLocation = async (id) => {
-  validateObjectId(id, "Location Id");
-  const result = await Location.findById(id);
+exports.deleteLocation = async (userId, locationId) => {
+  validateObjectId(locationId, "Location Id");
+  const result = await Location.findById(locationId);
   if (!result || result.isDeleted) throwError(404, "Location not found");
+  if (result?.userId?.toString() !== userId.toString()) {
+    throwError(403, "Unauthorized access");
+  }
   result.isDeleted = true;
   result.isActive = false;
   result.updatedAt = new Date();
